5ca5b5856d460249a39c890a63fa2a80661179b1
LuckyLukic <lucabjjiannice@gmail.com>
2023-07-14 15:50:31 +0200

    implementazione utente, prestito e relazione

--- src/main/java/Classi/Prestito.java
+++ src/main/java/Classi/Prestito.java
@@ -2,13 +2,35 @@ package Classi;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Prestiti")
+@Getter
+@Setter
+@NoArgsConstructor
+
+
 public class Prestito {
 	
-	Utente utente;
-	ModelloBase modello;
-	LocalDate inizioPrestito;
-	LocalDate ritornoPrevisto;
-	LocalDate finePrestito;
+	@Id
+	@SequenceGenerator(name = "My_Sequence", sequenceName = "My_Sequence", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "My_Sequence")
+	long id;
+	private Utente utente;
+	private ModelloBase modello;
+	private LocalDate inizioPrestito;
+	private LocalDate ritornoPrevisto;
+	private LocalDate finePrestito;
 	
 	public Prestito(Utente utente, ModelloBase modello, LocalDate inizioPrestito, LocalDate ritornoPrevisto,
 			LocalDate finePrestito) {
--- src/main/java/Classi/Utente.java
+++ src/main/java/Classi/Utente.java
@@ -3,11 +3,21 @@ package Classi;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "Utenti")
+@Getter
+@Setter
+@NoArgsConstructor
 
 
 public class Utente {
@@ -15,9 +25,12 @@ public class Utente {
 	String nome;
 	String cognome;
 	LocalDate dataNascita;
-	Long numeroTessera;
+	@Id
+	@SequenceGenerator(name = "My_New_Sequence", sequenceName = "My_New_Sequence", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "My_New_Sequence")
+	long numeroTessera;
 	
-	public Utente(String nome, String cognome, LocalDate dataNascita, Long numeroTessera) {
+	public Utente(String nome, String cognome, LocalDate dataNascita, long numeroTessera) {
 		this.nome = nome;
 		this.cognome = cognome;
 		this.dataNascita = dataNascita;
