bf63762e286ea37d2a97102484095baab56e6a50
LuckyLukic <lucabjjiannice@gmail.com>
2023-07-14 10:50:04 +0200

    jpaUtil e main

---
+++ src/main/java/Util/JpaUtil.java
@@ -0,0 +1,26 @@
+package Util;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaUtil {
+	
+		
+		private static final EntityManagerFactory entityManagerFactory;
+
+		static {
+			try {
+				entityManagerFactory = Persistence
+						.createEntityManagerFactory("U4-S3-D4");
+			} catch (Throwable ex) {
+				System.err.println("Initial EntityManagerFactory creation failed."
+						+ ex);
+				throw new ExceptionInInitializerError(ex);
+			}
+		}
+
+		public static EntityManagerFactory getEntityManagerFactory() {
+			return entityManagerFactory;
+		}
+
+}
